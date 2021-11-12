fe1a63027371b8a3e36a67729a71298786f30929
Stephanie Helm <stephaniemhelm@gmail.com>
2021-11-12 15:50:53 -0700

    extension

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'doctors/:id',          to: 'doctors#show'
-  get 'hospitals/:id',        to: 'hospitals#show'
-
+  get '/doctors/:id',          to: 'doctors#show'
+  get '/hospitals/:id',        to: 'hospitals#show'
+  get '/patients',             to: 'patients#index'
   delete '/doctors/:doctor_id/patients/:patient_id',   to: 'doctor_patients#destroy'
 
 end
--- spec/features/patients/index_spec.rb
+++ spec/features/patients/index_spec.rb
@@ -1,20 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Patient do
-  describe 'relationships' do
-    it { should have_many(:doctor_patients) }
-    it { should have_many(:doctors).through(:doctor_patients) }
-  end
-
-  it 'can order patients from oldest to youngest' do
+  it 'can list patient names and ages from oldest to youngest' do
     patient1 = Patient.create(name: "Hannah", age: 7)
     patient2 = Patient.create(name: "Sandy", age: 35)
     patient3 = Patient.create(name: "Rick", age: 60)
     patient4 = Patient.create(name: "Harlo", age: 12)
     patient5 = Patient.create(name: "Jason", age: 26)
 
-    expect(Patient.age_order).to eq([patient3, patient2, patient5, patient4, patient1])
+    visit '/patients'
 
+    expect(page).to have_content(patient1.name)
+    expect(page).to have_content(patient1.age)
   end
-
 end
